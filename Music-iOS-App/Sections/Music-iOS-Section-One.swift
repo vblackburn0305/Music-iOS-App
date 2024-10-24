45b7c84e085891f305e8d63a4842e1e2892a3dfd
Vincent-Blackburn <vblackburn0305@gmail.com>
2024-10-24 15:47:34 -0400

    Made the frame one section its own class and called that in main.

--- Music-iOS-App/Sections/Music-iOS-Section-One.swift
+++ Music-iOS-App/Sections/Music-iOS-Section-One.swift
@@ -1,5 +1,5 @@
 //
-//  Music-iOS-Section-Two.swift
+//  Music-iOS-Section-One.swift
 //  Music-iOS-App
 //
 //  Created by Vincent Blackburn on 10/24/24.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Music_iOS_Section_Two: View {
+struct Music_iOS_Section_One: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Music_iOS_Section_Two()
+    Music_iOS_Section_One()
 }
