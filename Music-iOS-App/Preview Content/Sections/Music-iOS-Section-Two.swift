1a2ca25973512384780ce44d7ec5b9eedeaf7829
Vincent-Blackburn <vblackburn0305@gmail.com>
2024-10-24 14:46:21 -0400

    Created a SectionsTwo file that will replace the current frame. Made to help code flow

---
+++ Music-iOS-App/Preview Content/Sections/Music-iOS-Section-Two.swift
@@ -0,0 +1,18 @@
+//
+//  Music-iOS-Section-Two.swift
+//  Music-iOS-App
+//
+//  Created by Vincent Blackburn on 10/24/24.
+//
+
+import SwiftUI
+
+struct Music_iOS_Section_Two: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Music_iOS_Section_Two()
+}
