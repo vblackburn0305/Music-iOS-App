ec1fb923b9caff3060679a00c28de576291ea005
Vincent-Blackburn <vblackburn0305@gmail.com>
2025-01-22 11:23:49 -0500

    added comments and documentation to files

--- Music-iOS-App/Music-iOS-App-Tab-View.swift
+++ Music-iOS-App/Music-iOS-App-Tab-View.swift
@@ -1,17 +1,17 @@
 import SwiftUI
 
-struct Music_iOS_App_Tab_View: View {
+struct Music_iOS_App_Tab_View: View { // create a view for changing scenes
     
-    @State var selectedTab: Int = 0
+    @State var selectedTab: Int = 0 // change the view based in the selectedTab var
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Music_iOS_App_Home(selectedTab: $selectedTab)
+        TabView(selection: $selectedTab) { // tab based navigation and link selection to selectedTab
+            Music_iOS_App_Home(selectedTab: $selectedTab) // display the home view
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Home")
                 }
-                .tag(0)
+                .tag(0) // change the active tab
             
             Music_iOS_App_Read()
                 .tabItem {
@@ -30,7 +30,7 @@ struct Music_iOS_App_Tab_View: View {
         }
     }
 }
-
+// preview for development
 #Preview {
     Music_iOS_App_Tab_View()
 }
